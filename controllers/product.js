4708e07afcb77a431217fdb4289611bc686ae5a5
Roshan <roshan.jha@hih7.in>
2023-06-30 18:13:23 +0530

    Add Pagination

--- controllers/product.js
+++ controllers/product.js
@@ -27,9 +27,16 @@ const getAllProducts = async (req, res) => {
     apiData = apiData.select(selectFix);
   }
 
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 3;
+
+  let skip = (page - 1) * limit;
+
+  apiData = apiData.skip(skip).limit(limit);
+
   const product = await apiData;
 
-  res.status(200).json({ product });
+  res.status(200).json({ product, nbHits: product.length });
 };
 //- for desc by default aesc
 
