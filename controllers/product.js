7540f6998629cbf96833e16fa67ae8f3ef0c8933
Roshan <roshan.jha@hih7.in>
2023-06-30 14:37:37 +0530

    Add Filtration & Searching Functionality with Query Props

--- controllers/product.js
+++ controllers/product.js
@@ -1,14 +1,10 @@
 const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-  const product = await Product.find();
-  res.status(200).json(product);
-};
-const getAllProductsTesting = async (req, res) => {
-  res.status(200).json({ msg: "I am get All Products for testing" });
+  const product = await Product.find(req.query);
+  res.status(200).json({ product });
 };
 
 module.exports = {
   getAllProducts,
-  getAllProductsTesting,
 };
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,8 @@
 const express = require("express");
-const {
-  getAllProducts,
-  getAllProductsTesting,
-} = require("../controllers/product");
+const { getAllProducts } = require("../controllers/product");
 
 const router = express.Router();
 
 router.route("/").get(getAllProducts);
-router.route("/testing").get(getAllProductsTesting);
 
 module.exports = router;
